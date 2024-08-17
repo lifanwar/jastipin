23efda9ac5caf5ac7d19212e7ae5ff11e651b15e
Lian exidosa <52408309+lifanwar@users.noreply.github.com>
2024-08-18 01:40:17 +0300

    add value search jastipin

---
+++ search/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      purple: "#4855DC",
+      abuthird: "#FCFBFB",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+    },
+  },
+};
